619aedb0c237b4af868e28a25f410fe2c680cc2b
0berwolf <werewolf01051990@gmail.com>
2023-01-19 16:05:49 +0300

    Создать контейнер на базе связанного списка

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,78 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+    private Node<E> last;
+
+    @Override
+    public void add(E value) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<E>(l, value, null);
+        last = newNode;
+        if (l == null) {
+            head = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> result = startGet(index, 0, head);
+        return result.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            Node<E> node = head;
+            final int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return node != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E value = node.item;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private Node<E> startGet(int index, int countStart, Node<E> accumulator) {
+        if (index > countStart) {
+            return startGet(index, countStart + 1, accumulator.next);
+        }
+        return accumulator;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
