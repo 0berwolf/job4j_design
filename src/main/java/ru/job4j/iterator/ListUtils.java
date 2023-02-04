3c76f5e6ae4adaad4c0f526328c4413beca4bbba
0berwolf <werewolf01051990@gmail.com>
2023-02-04 17:18:39 +0300

    ListIterator

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -6,26 +6,18 @@ import java.util.function.Predicate;
 public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
+        ListIterator<T> iterator = list.listIterator(index);
+            if (iterator.hasNext()) {
                 iterator.add(value);
-                break;
             }
-            iterator.next();
-        }
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.previousIndex() == index) {
+        ListIterator<T> iterator = list.listIterator(index + 1);
+            if (iterator.hasNext()) {
                 iterator.add(value);
-                break;
             }
-            iterator.next();
-        }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
@@ -47,11 +39,7 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> iterator = list.listIterator();
-        for (T element : elements) {
-            if (iterator.next() == element) {
-                iterator.remove();
-            }
-        }
+        Predicate<T> predicate = e -> elements.contains(e);
+        removeIf(list, predicate);
     }
 }
--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 class ListUtilsTest {
-
     private List<Integer> input;
 
     @BeforeEach
