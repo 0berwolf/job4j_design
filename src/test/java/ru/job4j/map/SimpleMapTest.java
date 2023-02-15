75ee9e25d5bef6c36e85725336a7c292246553f9
0berwolf <werewolf01051990@gmail.com>
2023-02-15 14:39:39 +0300

    Реализовать собственную структуру данных - HashMap

--- src/test/java/ru/job4j/map/SimpleMapTest.java
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -6,7 +6,6 @@ import java.util.*;
 import static org.assertj.core.api.Assertions.*;
 
 class SimpleMapTest {
-
     private final SimpleMap<Integer, String> map = new SimpleMap<>();
 
     @BeforeEach
