9671a31c7451aad0515338b66ced57f7ef4434f1
0berwolf <werewolf01051990@gmail.com>
2023-05-15 21:57:20 +0300

    Анализ доступности сервера

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,34 +1,18 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> rsl = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                rsl.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            for (int i = 0; i < rsl.size(); i++) {
-                String value = rsl.get(i);
-                String valueNext = ((i + 1) < rsl.size()) ? rsl.get(i + 1) : (null);
-                if (value.startsWith("400") || value.startsWith("500")) {
-                    String[] array = value.split(" ");
-                    out.print(array[1] + "; ");
-                    if ((valueNext != null && !valueNext.startsWith("400"))
-                            || (valueNext != null && !valueNext.startsWith("500"))) {
-                        String[] arrayNext = valueNext.split(" ");
-                        out.println(arrayNext[1] + ";");
-                    }
+        boolean workUnavailable = false;
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            String s;
+            while ((s = in.readLine()) != null) {
+                String[] splitLine = s.split(" ");
+                if (("400".equals(splitLine[0]) || "500".equals(splitLine[0])) != workUnavailable) {
+                    workUnavailable = !workUnavailable;
+                    out.append(splitLine[1]).append(";").append(workUnavailable ? "" : System.lineSeparator());
                 }
             }
         } catch (IOException e) {
