518e93ff8504d7904c62c990dc802aeaac496baf
0berwolf <werewolf01051990@gmail.com>
2023-07-07 14:26:16 +0300

    Файлы и директории. Класс File

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("Disc size : %s gb", file.getTotalSpace() / 1073741824));
+        for (File subfile : file.listFiles()) {
+            System.out.println(String.format(subfile.getName() + ", size : %s", subfile.length() / 1024 + "kb"));
+
+        }
+    }
+}
