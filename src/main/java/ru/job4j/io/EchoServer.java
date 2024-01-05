85326bab37f094cecb46352aba9fecc2997172ec
0berwolf <werewolf01051990@gmail.com>
2024-01-05 16:13:49 +0300

    Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -10,18 +10,18 @@ public class EchoServer {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream output = socket.getOutputStream();
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream()))) {
-                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String s = in.readLine();
-                    if (s.contains("?msg=Exit")) {
+                     BufferedReader input = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    output.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    String s = input.readLine();
+                    if (s.contains("msg=Exit")) {
                         output.write("Exit".getBytes());
                         server.close();
                     } else {
-                        output.write(s.contains("?msg=Hello") ? "Hello".getBytes() : "What".getBytes());
+                        output.write(s.contains("msg=Hello") ? "Hello".getBytes() : "What".getBytes());
                     }
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                            System.out.println(str);
+                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
+                        System.out.println(string);
                     }
                     output.flush();
                 }
