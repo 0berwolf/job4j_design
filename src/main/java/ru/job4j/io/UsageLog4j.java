af8614df1822ee39db8d2e505f373b7958ba6d8b
0berwolf <werewolf01051990@gmail.com>
2024-02-02 14:30:27 +0300

    Slf4j - вывод переменных

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Andrei Golub";
+        int age = 33;
+        byte jobAgeStart = 23;
+        short lengthOfService = 10;
+        long salary = 1328L;
+        float workingDay = 11.5f;
+        double yearlyWorkingHours = 2040.5;
+        boolean work = true;
+        char n = 'Y';
+        LOG.debug("User info name : {}, age : {}, jobAgeStart : {}, lengthOfService : {}, salary : {}, workingDay : {}, "
+                        + "yearlyWorkingHours : {}, work : {}, n : {}", name, age, jobAgeStart, lengthOfService, salary, workingDay,
+                yearlyWorkingHours, work, n);
     }
 }
